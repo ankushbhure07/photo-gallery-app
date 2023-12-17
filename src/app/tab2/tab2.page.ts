906681e49112780c48d5f989f69987fbeb24f262
Ankush Bhure <53933685+ankushbhure07@users.noreply.github.com>
2023-12-17 21:16:29 +0530

    Initial Upload

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { PhotoService } from '../services/photo.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: true,
+  imports: [IonicModule, ExploreContainerComponent, CommonModule]
+})
+export class Tab2Page implements OnInit {
+
+  constructor(public photoService: PhotoService) { }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+
+    setTimeout(() => {
+      console.log(this.photoService.photos)
+    }, 2000)
+  }
+
+}
